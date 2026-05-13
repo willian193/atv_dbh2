26958a21c1d8a8bdac994ab797c72487f2a6b323
willian193 <willianradunzz24@gmail.com>
2026-05-13 08:45:43 -0300

    Initial commit

---
+++ src/main/java/com/example/atv_dbh2/AtvDbh2Application.java
@@ -0,0 +1,13 @@
+package com.example.atv_dbh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtvDbh2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtvDbh2Application.class, args);
+	}
+
+}
